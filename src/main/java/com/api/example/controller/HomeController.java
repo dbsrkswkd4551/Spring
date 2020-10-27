d473da6f6309ab1bbf16c9f072a0689dad955609
dbsrkswkd4551 <ysang4512005@naver.com>
2020-10-27 14:22:38 +0900

    ch2. 프로젝트 생성 및 freemarker를 이용한 helloworld 출력

--- src/main/java/com/api/example/controller/HomeController.java
+++ src/main/java/com/api/example/controller/HomeController.java
@@ -1,4 +1,37 @@
 package com.api.example.controller;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+    @GetMapping(value = "/helloworld/string")
+    @ResponseBody
+    public String helloworldString(){
+        return "helloworld";
+    }
+
+    @GetMapping(value = "/helloworld/json")
+    @ResponseBody
+    public Hello helloworldJson() {
+        Hello hello = new Hello();
+        hello.message = "helloworld";
+        return hello;
+    }
+
+    @GetMapping(value = "/helloworld/page")
+    public String helloworld(){
+        return "helloworld";
+    }
+
+    @Setter
+    @Getter
+    public static class Hello{
+        private String message;
+    }
 }
