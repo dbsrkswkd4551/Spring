cce3e1d593d545f125853eb081ba5bfec3f1072e
dbsrkswkd4551 <ysang4512005@naver.com>
2020-10-29 09:55:03 +0900

    ch3. View 설정

--- src/main/java/com/api/example/controller/HomeController.java
+++ src/main/java/com/api/example/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.api.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -17,6 +21,20 @@ public class HomeController {
     public String data(){
         String value = "hello data";
         return value;
+    }
+
+    @RequestMapping("/test")
+    public ModelAndView test() throws Exception{
+        ModelAndView mav = new ModelAndView("test");
+        mav.addObject("name", "Yun");
+
+        List<String> testList = new ArrayList<String>();
+        testList.add("a");
+        testList.add("b");
+        testList.add("c");
+
+        mav.addObject("list", testList);
+        return mav;
     }
 
 }
